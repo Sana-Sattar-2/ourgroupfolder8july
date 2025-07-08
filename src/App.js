bdf458a8869d381536310171f9109fcacc20d0fc
Sana-Sattar <sanasattar057@gmail.com>
2025-07-08 18:30:31 +0500

    sana

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react'
-import  Navbar  from './component/navbar/Navbar'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Navbar from './component/navbar/Navbar';
+import Home from './pages/home/Home';
+import Furniture from './pages/furniture/Furniture';
+import About from './pages/about/About';
+import Contact from './pages/contact/ContactUS';
+import Blog from './pages/blog/Blog';
+import Testimonial from './pages/testimonial/Testimonial';
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/furniture" element={<Furniture />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/testimonial" element={<Testimonial />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
